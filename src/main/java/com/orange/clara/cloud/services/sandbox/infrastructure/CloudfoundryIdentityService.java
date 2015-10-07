0200100cf3ee89149cea0d8027825c026cb0dff8
Olivier ORAND <olivier.orand@orange.com>
2015-10-07 15:13:47 +0200

    use JwtHelper to parse oAuth2 token and pass var env to docker

--- src/main/java/com/orange/clara/cloud/services/sandbox/infrastructure/CloudfoundryIdentityService.java
+++ src/main/java/com/orange/clara/cloud/services/sandbox/infrastructure/CloudfoundryIdentityService.java
@@ -14,21 +14,23 @@
 
 package com.orange.clara.cloud.services.sandbox.infrastructure;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.orange.clara.cloud.services.sandbox.domain.IdentityService;
 import com.orange.clara.cloud.services.sandbox.domain.UserInfo;
 import org.cloudfoundry.client.lib.CloudFoundryClient;
-import org.cloudfoundry.client.lib.util.JsonUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
-import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.jwt.Jwt;
+import org.springframework.security.jwt.JwtHelper;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.security.Principal;
-import java.util.Map;
+import java.util.HashMap;
 
 /**
  * Created by sbortolussi on 05/10/2015.
@@ -37,55 +39,40 @@ import java.util.Map;
 @Scope(value = "session", proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class CloudfoundryIdentityService implements IdentityService {
 
-    private OAuth2ClientContext oauth2Context;
-
     private CloudFoundryClient cloudFoundryClient;
+    private OAuth2AccessToken oAuth2AccessToken;
 
     @Autowired
-    public CloudfoundryIdentityService(@Qualifier("cloudFoundryClientAsUser") CloudFoundryClient cloudFoundryClient, OAuth2ClientContext oauth2Context) {
+    public CloudfoundryIdentityService(@Qualifier("cloudFoundryClientAsUser")CloudFoundryClient cloudFoundryClient) {
         this.cloudFoundryClient = cloudFoundryClient;
-        this.oauth2Context = oauth2Context;
     }
+
+    @Autowired
+    public CloudfoundryIdentityService setoAuth2AccessToken(OAuth2AccessToken oAuth2AccessToken) {
+        this.oAuth2AccessToken = oAuth2AccessToken;
+        return this;
+    }
+
 
     @Override
-    public UserInfo getInfo(Principal principal) {
-        //TO force user info from ldap loading into CC
-        final String username = cloudFoundryClient.getCloudInfo().getUser();
-        UserInfo userInfo = new UserInfo(username,getCurrentUserId(username));
-        //UserInfo userInfo = new UserInfo(userPrincipal.getName(), getCurrentUserId(userPrincipal.getName()));
+    public UserInfo getInfo(Principal userPrincipal) {
 
-        return userInfo;
-    }
+        createUserInCloudfoundry();
 
-    private Map<String, Object> getUserInfo(String user) {
-//		String userJson = getRestTemplate().getForObject(getUrl("/v2/users/{guid}"), String.class, user);
-//		Map<String, Object> userInfo = (Map<String, Object>) JsonUtil.convertJsonToMap(userJson);
-//		return userInfo();
-        //TODO: remove this temporary hack once the /v2/users/ uri can be accessed by mere mortals
-        String userJson = "{}";
-        OAuth2AccessToken accessToken = oauth2Context.getAccessToken();
-        if (accessToken != null) {
-            String tokenString = accessToken.getValue();
-            int x = tokenString.indexOf('.');
-            int y = tokenString.indexOf('.', x + 1);
-            String encodedString = tokenString.substring(x + 1, y);
-            try {
-                byte[] decodedBytes = new sun.misc.BASE64Decoder().decodeBuffer(encodedString);
-                userJson = new String(decodedBytes, 0, decodedBytes.length, "UTF-8");
-            } catch (IOException e) {
-            }
+        Jwt jwt=JwtHelper.decode(oAuth2AccessToken.getValue());
+        HashMap<String,Object> map;
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            map=mapper.readValue(jwt.getClaims(), new TypeReference<HashMap<String,Object>>(){});
+            UserInfo userInfo = new UserInfo(userPrincipal.getName(), (String) map.get("user_id"));
+            return userInfo;
+        } catch (IOException e) {
+            throw  new RuntimeException("Cannot parse jwt token for user " +userPrincipal.getName(),e);
         }
-        return (JsonUtil.convertJsonToMap(userJson));
+
     }
 
-    private String getCurrentUserId(String username) {
-        Map<String, Object> userMap = getUserInfo(username);
-        String userId = (String) userMap.get("user_id");
-        return userId;
+    private void createUserInCloudfoundry() {
+        cloudFoundryClient.getCloudInfo();
     }
-    /*
-         private String getCurrentUserId(String username) {
-        return String.valueOf(oauth2Context.getAccessToken().getAdditionalInformation().get("user_id"));
-    }
-     */
 }
