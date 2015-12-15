3988212bd4fb6c31b37c21a9a83517a892c0cdb0
Olivier ORAND <olivier.orand@orange.com>
2015-12-15 23:08:38 +0100

    chore: try to add config to allow commit from travis to Github
    Add GH_USER, GH_EMAIL and GH_TOKEN (previously GH_TAGPERM)

--- bin/release.sh
+++ bin/release.sh
@@ -23,7 +23,7 @@ then
 	export RELEASE_NAME=$(expr "$RELEASE_CANDIDATE_VERSION" : "\(.*\)-SNAP.*")
 	export TAG_DESC="Generated tag from TravisCI for build $TRAVIS_BUILD_NUMBER - $TAG_NAME. Binaries available as maven dependencies "'[ ![Set me Up !](https://img.shields.io/badge/JCenter Bintray-Set me Up !-orange.svg) ] (https://bintray.com/package/buildSettings?pkgPath=/elpaaso/maven/'"$REPO_NAME)and also here "'[ ![Download](https://img.shields.io/badge/Download-'$RELEASE_NAME'-blue.svg) ](https://bintray.com/elpaaso/maven/'"$REPO_NAME/$RELEASE_NAME/)"
 	export GITHUB_DATA='{"tag_name":"'$TAG_NAME'","target_commitish":"master","name":"'"$RELEASE_NAME"'","body":"'"$TAG_DESC"'","draft": true,"prerelease": true}'
-	curl --silent -X POST --data "$GITHUB_DATA" https://$GH_TAGPERM@api.github.com/repos/Orange-OpenSource/$REPO_NAME/releases
+	curl --silent -X POST --data "$GITHUB_DATA" https://$GH_TOKEN@api.github.com/repos/Orange-OpenSource/$REPO_NAME/releases
 
 	echo "Extracted Travis repo name: $REPO_NAME"
 
